205733befc422f135c919a45d4e7f1d84bc0ee28
NATANAEL ALVES PINTO <natan@NATANAELs-MacBook-Pro.local>
2022-12-06 00:38:57 -0300

    End point post and get w/ header

--- Program.cs
+++ Program.cs
@@ -2,5 +2,27 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/user", () => "Natan");
+app.MapPost("/user", () => new { Name = "Natan Alves", Age = 40 });
+app.MapGet("/Add", (HttpResponse response) =>
+{
+    response.Headers.Add("Teste", "Natan Alves");
+    return new
+    {
+        Name = "Natan Alves",
+        Age = 40
+    };
+});
+
+app.MapPost("/saveprod", (Product product) =>
+{
+    return product.Code + " - " + product.Name;
+});
 
 app.Run();
+
+public class Product
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+}
